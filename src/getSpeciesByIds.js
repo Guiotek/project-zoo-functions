83be9cc169cd48748cc231106b7d040f5c203e2d
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-07-28 18:50:19 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  return ids.map((elementId) => species.find((specie) => specie.id === elementId));
 }
+
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
