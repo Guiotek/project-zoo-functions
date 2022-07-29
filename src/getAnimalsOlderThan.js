882f5eec630fc63916d8758c3805b0654c7e60aa
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-07-29 14:11:19 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specie = species.find((element) => element.name === animal);
+  const specieAges = specie.residents.filter((element) => element.age);
+  const verifyAges = specieAges.every((element) => element.age !== age);
+  return verifyAges;
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
