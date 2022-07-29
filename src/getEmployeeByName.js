f6680d29f723853a95472ee745ad1f23a2f85fed
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-07-29 14:53:12 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,24 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const employee = employees.find((element) => {
+    if (element.firstName === employeeName) {
+      return element.firstName;
+    }
+    if (element.lastName === employeeName) {
+      return element.lastName;
+    }
+  });
+  return employee;
 }
+
+console.log(getEmployeeByName('Nigel'));
+console.log(getEmployeeByName('Nelson'));
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
