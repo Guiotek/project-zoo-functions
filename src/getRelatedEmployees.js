621cae53114fdd7091261d91b86ea097f85ad9d5
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-07-31 15:17:22 -0300

    requisito de test

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,21 +1,13 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  const employee = employees.find((element) => element.id === id);
-  const n = '4b40a139-d4dc-4f09-822d-ec25e819a5ad';
-  const employeeIsManager = employees.some((element) => element.id !== n);
-  console.log(employeeIsManager);
-  return employeeIsManager;
 }
 
 console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-  const employee = employees.find((element) => element.id === managerId);
-  return employee.managers;
 }
 
 console.log(getRelatedEmployees('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
