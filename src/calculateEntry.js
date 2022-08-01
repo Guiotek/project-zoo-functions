14695a39d2262f7b5967c36f2635cc0b9a2700e5
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-01 14:09:39 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,40 @@
 const data = require('../data/zoo_data');
 
+const entrant = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aqui.
+  let c = 0;
+  let a = 0;
+  let s = 0;
+  const count = (pessoa) => {
+    if (pessoa.age < 18) { c += 1; }
+    if (pessoa.age >= 18 && pessoa.age < 50) { a += 1; }
+    if (pessoa.age >= 50) { s += 1; }
+  };
+  entrants.forEach((item) => count(item));
+  return { child: c, adult: a, senior: s };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === 0 || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const pessoas = countEntrants(entrants);
+  let valor = 0;
+  const c = pessoas.child * 20.99;
+  const a = pessoas.adult * 49.99;
+  const s = pessoas.senior * 24.99;
+  valor = c + a + s;
+  return valor;
 }
+
+console.log(calculateEntry(entrant));
 
 module.exports = { calculateEntry, countEntrants };
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,9 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+function getEmployeesCoverage(object) {
   // seu código aqui
+
 }
 
 module.exports = getEmployeesCoverage;
