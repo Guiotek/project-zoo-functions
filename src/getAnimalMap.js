dec85f22df13c98580a6722f6226114f536fa07a
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-01 18:01:31 -0300

    requisito 5

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+}
 
 module.exports = getAnimalMap;
