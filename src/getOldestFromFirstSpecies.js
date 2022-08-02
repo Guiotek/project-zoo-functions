ca8c17694f6d3000e83c8029fd9c6755ecfa52d9
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-02 15:58:40 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employ = employees.find((element) => element.id === id);
+  const firstAnimal = employ.responsibleFor[0];
+  const animal = species.find((element) => element.id === firstAnimal);
+  const verifyAge = animal.residents.sort((a, b) => b.age - a.age);
+  return [verifyAge[0].name, verifyAge[0].sex, verifyAge[0].age];
 }
+
+console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,12 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const animal = species.find((element) => element.name === scheduleTarget);
+  return animal;
 }
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
