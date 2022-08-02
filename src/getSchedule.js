5e6a122462cd050148d135542f2f0438ecfc0176
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-02 16:03:09 -0300

    requisito 10

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,4 +1,4 @@
-const { species, hours } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
