1d5cb9b23c9d38e9911b7503389ab486b6b02525
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-03 13:30:23 -0300

    requisito 4

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,7 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeesCoverage(object) {
   // seu código aqui
-
 }
+
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,15 +1,38 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  const managers = [stephanieId, olaId, burlId];
+  let initial = false;
+  const verifyManager = (managerId) => {
+    if (id === managerId) {
+      initial = true;
+    }
+  };
+  managers.forEach((managerId) => verifyManager(managerId));
+  return initial;
 }
-
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const trueAndFalse = isManager(managerId);
+  if (trueAndFalse === true) {
+    let array = [];
+    const a = (employee) => {
+      employee.managers.forEach((element) => {
+        if (element === managerId) {
+          array.push(`${employee.firstName} ${employee.lastName}`);
+        }
+      });
+    };
+    employees.forEach((employee) => a(employee));
+    return array;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
-console.log(getRelatedEmployees('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = { isManager, getRelatedEmployees };
